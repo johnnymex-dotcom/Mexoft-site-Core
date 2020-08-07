b5dc66725a776bece22bfdd69adb821d4c9da741
Joo <jools@mst.dk>
2020-08-07 13:47:28 +0200

    Adjusted CV properties

---
+++ Models/MexLog.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.Web.CodeGeneration.Templating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mexoft_site.Models
+{
+    public class MexLog
+    {
+        private readonly RequestDelegate _next;
+        private IHttpContextAccessor accessor;
+
+        public MexLog(RequestDelegate next, IHttpContextAccessor _accessor)
+        {
+            _next = next;
+            accessor = _accessor;
+        }
+
+        private async Task<string> GetIP()
+        {
+            var ip = accessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            System.Threading.Thread.Sleep(20000);
+
+            return await Task.FromResult<string>(ip);
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var req = context.Request.Path;
+            if (req.Value == "/home/cvmain") 
+            {
+                //var ip = await GetIP();
+            
+            }
+            await _next(context);
+
+        }
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mexoft_site.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,11 +27,15 @@ namespace Mexoft_site
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMiddleware<MexLog>();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
