5049ceaf4939b58762a6c95b4b804be47f3db721
jooflyngby@gmail.com <jooflyngby@gmail.com>
2021-11-25 16:54:09 +0100

    Leaving stash

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -111,6 +111,12 @@ namespace Mexoft_site.Controllers
             return View();
         }
 
+        public ActionResult GlobalizationIssue()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
